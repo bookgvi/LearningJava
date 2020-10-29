52a5968a8f6b97ab9c30489658ea60a0fbc54a47
bookgvi <bookgvi@gmail.com>
2020-10-29 20:49:51 +0300

    some discovery with reflection

--- src/Inheritance/Square.java
+++ src/Inheritance/Square.java
@@ -1,7 +1,32 @@
 package Inheritance;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 public class Square extends Shape {
+  private String shapeClassField;
+
   public String getSomeVar() {
     return this.someVar;
+  }
+
+  public void checkProperties() throws ClassNotFoundException {
+    Class squareClass = Class.forName(Square.class.getName());
+    Field[] declaredFields = squareClass.getDeclaredFields();
+    Field[] fields = squareClass.getFields();
+
+    System.out.printf("SuperClass: %s%n", squareClass.getSuperclass().getName());
+    System.out.printf("Declared fields count: %s%n", declaredFields.length);
+    if (declaredFields.length > 0) {
+      Arrays.stream(declaredFields).forEach(field -> {
+        System.out.println(field.getName());
+      });
+    }
+    System.out.printf("Fields count: %s%n", fields.length);
+    if (fields.length > 0) {
+      Arrays.stream(fields).forEach(field -> {
+        System.out.println(field.getName());
+      });
+    }
   }
 }
--- src/InheritanceApp.java
+++ src/InheritanceApp.java
@@ -1,8 +1,9 @@
 import Inheritance.Square;
 
 public class InheritanceApp {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException {
     Square square = new Square();
     System.out.printf("%s%n", square.getSomeVar());
+    square.checkProperties();
   }
 }
