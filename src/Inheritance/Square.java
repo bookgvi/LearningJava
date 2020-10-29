2ffa42b82771028f7aadfde88fb28fa33397bb74
bookgvi <bookgvi@gmail.com>
2020-10-29 21:21:09 +0300

    Исследование полей класса через Reflection API

--- src/Inheritance/Square.java
+++ src/Inheritance/Square.java
@@ -1,32 +1,46 @@
 package Inheritance;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 public class Square extends Shape {
-  private String shapeClassField;
+  private String shapeClassField = "QQQ";
 
   public String getSomeVar() {
     return this.someVar;
   }
 
-  public void checkProperties() throws ClassNotFoundException {
-    Class squareClass = Class.forName(Square.class.getName());
+  public void checkProperties() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    Class<?> squareClass = Class.forName(Square.class.getName());
+    Square square = this.createSquareObj(squareClass);
     Field[] declaredFields = squareClass.getDeclaredFields();
     Field[] fields = squareClass.getFields();
 
     System.out.printf("SuperClass: %s%n", squareClass.getSuperclass().getName());
     System.out.printf("Declared fields count: %s%n", declaredFields.length);
-    if (declaredFields.length > 0) {
-      Arrays.stream(declaredFields).forEach(field -> {
-        System.out.println(field.getName());
-      });
-    }
+    this.showFiledsInfo(declaredFields, square);
     System.out.printf("Fields count: %s%n", fields.length);
+    this.showFiledsInfo(fields, square);
+  }
+
+  private void showFiledsInfo(Field[] fields, Object obj) {
     if (fields.length > 0) {
       Arrays.stream(fields).forEach(field -> {
-        System.out.println(field.getName());
+        try {
+          System.out.printf("Name: %s, type: %s, value = %s%n", field.getName(), field.getType().getName(), field.get(obj));
+        } catch (Exception ignored) {
+        }
       });
+    } else {
+      System.out.println("Empty...");
     }
+  }
+
+  private Square createSquareObj(Class<?> clazz) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    Square square = null;
+    Class[] params = new Class[] {};
+    square = (Square) clazz.getConstructor(params).newInstance();
+    return square;
   }
 }
--- src/InheritanceApp.java
+++ src/InheritanceApp.java
@@ -1,7 +1,9 @@
 import Inheritance.Square;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class InheritanceApp {
-  public static void main(String[] args) throws ClassNotFoundException {
+  public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
     Square square = new Square();
     System.out.printf("%s%n", square.getSomeVar());
     square.checkProperties();
