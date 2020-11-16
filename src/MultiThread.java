512c8c665e4925165782f727686839118795c52c
bookgvi <bookgvi@gmail.com>
2020-11-16 12:01:12 +0300

    feature - 'instantiation' abstract class

--- src/MultiThread.java
+++ src/MultiThread.java
@@ -7,6 +7,7 @@ public class MultiThread {
   private static final int THREAD_COUNT = 5;
   private static final int DELAY_BASE = 5;
 
+
   private static int delay() {
     Random rnd = new Random();
     int delay = rnd.nextInt(DELAY_BASE);
@@ -16,6 +17,9 @@ public class MultiThread {
   }
 
   public static void main(String[] args) throws InterruptedException {
+    AbstractClass abstractClass = new AbstractClass() {};
+    System.out.printf("Abstract: %s%n", abstractClass.someField);
+
     ThreadGroup threadGroup = new ThreadGroup("QQQ");
     List<MyRunnable> tasks = new ArrayList<>();
     Map<Integer, String> map = ConcurrentObj.getMap(true);
---
+++ src/MultiThread/AbstractClass.java
@@ -0,0 +1,5 @@
+package MultiThread;
+
+public abstract class AbstractClass {
+  public String someField = "QQQ";
+}
