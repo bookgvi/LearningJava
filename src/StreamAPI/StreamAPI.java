0fcb65ee9c947a317ff867a42061da981f6ff880
bookgvi <bookgvi@gmail.com>
2020-11-19 20:13:40 +0300

    Stream api

--- src/StreamAPI/StreamAPI.java
+++ src/StreamAPI/StreamAPI.java
@@ -2,6 +2,8 @@ package StreamAPI;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamAPI {
@@ -15,5 +17,10 @@ public class StreamAPI {
 
   public Stream<Integer> getStream() {
     return intList.stream();
+  }
+
+  public void square(ArrayList<Integer> arr) {
+    IntStream stream = arr != null ? arr.stream().mapToInt(i -> i) : new Random().ints();
+    stream.map(i -> i*i).forEach(System.out::print);
   }
 }
