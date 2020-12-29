509978f78b20a6c4f748c19fc3156addfd352169
bookgvi <bookgvi@gmail.com>
2020-12-29 22:04:42 +0300

    Singleton pattern - enum - разные конструкторы

--- src/Patterns/Singleton/SingletonEnum.java
+++ src/Patterns/Singleton/SingletonEnum.java
@@ -1,15 +1,23 @@
 package Patterns.Singleton;
 
 public enum SingletonEnum {
-  INSTANCE("This is SingletonEnum.class (enum)");
-  private String info;
+  INSTANCE("This is SingletonEnum.class (enum) - INSTANCE1"),
+  INSTANCE2,
+  INSTANCE3("This is SingletonEnum.class (enum) - INSTANCE3");
+  private String info = "QQQ";
 
   SingletonEnum(String info) {
     this.info = info;
   }
 
+  SingletonEnum() {
+
+  }
+
   public SingletonEnum getInstance() {
     return INSTANCE;
+//    return INSTANCE2;
+//    return INSTANCE3;
   }
 
   public String getInfo() {
